15aff8ca1367d551c52afc72cfb7cdf87e4d5e5b
Brendan <bwaldropw@aol.com>
2023-01-18 14:46:03 -0700

    deck class and testing

--- core/src/com/bwally/blackjack/Deck.java
+++ core/src/com/bwally/blackjack/Deck.java
@@ -4,17 +4,25 @@ import java.util.Collections;
 import java.util.Stack;
 
 public class Deck {
-    private Stack<Card> deck = new Stack<Card>();
-    private String[] faces = {"ACE", "ONE", "TWO", "THREE", "FOUR", "FIVE",
+    private final Stack<Card> deck = new Stack<Card>();
+    private final String[] faces = {"ACE", "TWO", "THREE", "FOUR", "FIVE",
             "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING"};
-    private String[] suits = {"CLUB", "SPADE", "DIAMOND", "HEART"};
+    private final String[] suits = {"CLUB", "SPADE", "DIAMOND", "HEART"};
 
     public Deck() {
         fillDeck();
+
     }
 
     private void fillDeck() {
-        deck.push(new Card(new Face("ACE"), new Suit("CLUB")));
+        for (int i = 0; i < faces.length; i++) {
+
+            for (int j = 0; j < suits.length; j++) {
+                deck.push(new Card(new Face(faces[i]), new Suit(suits[j])));
+            }
+        }
+
+        shuffle();
     }
 
     public void shuffle() {
@@ -22,11 +30,9 @@ public class Deck {
     }
 
     public void printDeck() {
-
-        System.out.println(deck.get(0).getFace().getName() + " " + deck.get(0).getSuit().getName());
-//        for (int i = 0; i < deck.size(); i++) {
-//            System.out.println(deck.get(i).getSuit().getName() + " " + deck.get(i).getFace().getName());
-//        }
+        for (int i = 0; i < deck.size(); i++) {
+            System.out.println(deck.get(i).getFace().getName() + " " + deck.get(i).getSuit().getName());
+        }
     }
 
 }
